41e8c37c9a1289abe5c880f72c1ecd1f1c51ed9b
luke <lfairbanks@anamosa.k12.ia.us>
2025-12-09 15:11:34 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function priceCheck(price) {
+  if (price == 1.50) {
+    return 'match';
+  }
+}
+
+function strictPriceCheck(price) {
+  if (price === 1.50) {
+    return 'match';
+  }
+}
+
+// expose to browser global (redundant in many environments but explicit)
+window.priceCheck = priceCheck;
+window.strictPriceCheck = strictPriceCheck;
 
